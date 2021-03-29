bd434c1f23111eb4893abf0017723ae2a47954a0
Abdullahi Odesanmi <56407386+realabdullah@users.noreply.github.com>
2021-03-29 15:23:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 const taskValue = document.getElementById("task-input");
 const addTask = document.getElementById("add-task");
+const taskDesc = document.getElementById("taskdetails");
 const deleteTas = document.querySelector(".close");
 const task = document.querySelector('#task');
 
 addTask.onclick = taskAdd;
 
 taskValue.onkeyup = () => {
-    if(taskValue.trim() != 0){
-        taskValue.classList.add("error");
+    var taskName = taskValue.value;
+    if(taskName.charAt(0) == " " || taskName == ""){
+        disableBtn();
+    } else {
+        enableBtn();
     }
+}
+
+function disableBtn(){
+    taskValue.style.borderBottom="2px solid red";
+    addTask.disabled = true;
+}
+
+function enableBtn(){
+    addTask.disabled = false;
 }
 
 function taskAdd(){
     let storedTask = localStorage.getItem("NewTask");
     if(storedTask == null){
-        taskArray = [];
+        taskArray, taskDescription = [];
     } else {
-        taskArray = JSON.parse(storedTask);
+        taskArray, taskDescription = JSON.parse(storedTask);
     }
     taskArray.push(taskValue.value);
-    localStorage.setItem("NewTask", JSON.stringify(taskArray));
+    taskDescription.push(taskDesc.value);
+    localStorage.setItem("NewTask", JSON.stringify(taskArray, taskDescription));
     showTasks();
-    console.log(taskArray);   
+    taskValue.value = "";
+    console.log(taskArray); 
+    console.log(taskDescription); 
 }
 
 function showTasks(){
@@ -40,7 +56,17 @@ function showTasks(){
 function deleteTask(index){
     let storedTask = localStorage.getItem("NewTask");
     taskArray = JSON.parse(storedTask);
-    taskArray.splice(index, 1);
-    localStorage.setItem("New Todo", JSON.stringify(taskArray));
+    
+    for(var i = 0; i < taskArray.length; i++){
+        taskArray.splice(index, 1);
+    }
+    localStorage.setItem("NewTask", JSON.stringify(taskArray));
     showTasks();
+    hidePane();
+}
+
+function hidePane(){
+    if(taskArray.length == 0){
+        document.querySelector('#tasks').style.display = "none";
+    }
 }
